40dda6abe42004c1661239daf0e5052372d0470d
touchy0912 <touchy0912@gmail.com>
2018-06-07 05:58:03 +0000

    CRUD tasks

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>新規メッセージ作成</h1>
+
+{!! Form::model($task,['route'=>'tasks.store']) !!}
+{!! Form::label('content','タスク:') !!}
+{!! Form::text('content') !!}
+{!! Form::submit('投稿') !!}
+
+{!! Form::close() !!}
+
+
+
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>id={{$task->id}}の詳細ページ</h1>
+
+<P>{{$task->content}}</P>
+
+{!! link_to_route('tasks.edit','編集する',['id'=>$task->id]) !!}
+
+{!! Form::model($task,['route'=>['tasks.destroy',$task->id],'method'=>'delete']) !!} 
+
+{!! Form::submit('削除') !!}
+{!! Form::close() !!}
+
+@endsection
