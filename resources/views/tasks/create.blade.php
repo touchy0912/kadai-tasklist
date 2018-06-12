78f6526f1a49f417486425701459ccb106ae37fa
touchy0912 <touchy0912@gmail.com>
2018-06-12 07:09:33 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,19 +4,23 @@
 
 <h1>新規メッセージ作成</h1>
 
-
+<div class='row'>
+    <div class='col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6'>
 {!! Form::model($task,['route'=>'tasks.store']) !!}
-
+<div class='format-group'>
 {!! Form::label('staus','ステータス:') !!}
-{!! Form::text('status') !!}
+{!! Form::text('status',null,['class'=>'form-control']) !!}
+</div>
 
+<div class='format-group'>
 {!! Form::label('content','タスク:') !!}
-{!! Form::text('content') !!}
-{!! Form::submit('投稿') !!}
+{!! Form::text('content',null,['class'=>'form-control']) !!}
+</div>
+{!! Form::submit('投稿',['class'=>'btn btn-primary']) !!}
 
 {!! Form::close() !!}
 
-
-
+</div>
+</div>
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,15 +3,32 @@
 @section('content')
 
 <h1>id={{$task->id}}の詳細ページ</h1>
+<table class='table table-bordered'>
 
-<p>ステータス：{{$task->status}}</p>
-<P>タスク：{{$task->content}}</P>
+<tr>
+    <td>id</td>
+    <td>{{$task->id}}</td>
+</tr>
 
-{!! link_to_route('tasks.edit','編集する',['id'=>$task->id]) !!}
+<tr>
+    <td>ステータス</td>
+    <td>{{$task->status}}</td>
+    
+</tr>
+
+<tr>
+    <td>タスク</td>
+    <td>{{$task->content}}</td>
+</tr>
+
+
+</table>
+
+{!! link_to_route('tasks.edit','編集する',['id'=>$task->id],['class'=>'btn btn-default']) !!}
 
 {!! Form::model($task,['route'=>['tasks.destroy',$task->id],'method'=>'delete']) !!} 
 
-{!! Form::submit('削除') !!}
+{!! Form::submit('削除',['class'=>'btn btn-danger']) !!}
 {!! Form::close() !!}
 
 @endsection
